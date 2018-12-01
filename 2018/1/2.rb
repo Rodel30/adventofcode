bc89c47dbcc665f83af46f07bafe414cab51333f
Isaiah Work <isaiah@singlebrook.com>
2018-11-30 23:52:24 -0600

    Add simplified better solution

--- 2018/1/2.rb
+++ 2018/1/2.rb
@@ -2,7 +2,7 @@ require 'set'
 def main
   f_vals = Set.new([0])
   f = 0
-  lines = File.readlines('i')
+  lines = File.readlines('i.txt')
   while true
     lines.each do |l|
       f = f.public_send(l[0], l[1..-1].to_i)
@@ -15,3 +15,18 @@ def main
 end
 
 puts main
+
+def better
+  f_vals = Set.new([0])
+  f = 0
+  lines = File.readlines('i.txt')
+  while true
+    lines.each do |l|
+      f += l.to_i
+      return f if f_vals.include? f
+      f_vals << f
+    end
+  end
+end
+
+puts better
