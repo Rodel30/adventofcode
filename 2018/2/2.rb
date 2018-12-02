f040b59493c0e9e1f9ce7b4c4efe1f0a2b221452
Isaiah Work <isaiah@singlebrook.com>
2018-12-01 23:43:30 -0600

    A couple improvements

--- 2018/2/2.rb
+++ 2018/2/2.rb
@@ -22,3 +22,31 @@ def main
 end
 
 puts main
+
+def better
+  lines = File.readlines('i.txt')
+  lines.count.times do |n|
+    l = lines[n]
+    ((n+1)..(lines.count-1)).each do |n2|
+      l2 = lines[n2]
+      if l2.length == l.length
+        diffs = l.length.times.reject{|cn| l[cn] == l2[cn] }
+        return l.tap{|s| s.slice! diffs[0] } if diffs.count == 1
+      end
+    end
+  end
+end
+
+puts better
+
+def betterer
+  lines = File.readlines('i.txt')
+  lines.product(lines).each do |l, l2|
+    if l2.length == l.length
+      diffs = l.length.times.reject{|cn| l[cn] == l2[cn] }
+      return l.tap{|s| s.slice! diffs[0] } if diffs.count == 1
+    end
+  end
+end
+
+puts betterer
